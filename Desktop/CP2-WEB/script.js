6fbf1e67168575cd37ffb628ad49340aedc7bfaf
Thiago <thiagodeboliveira@outlook.com>
2024-04-29 20:45:33 -0300

    Criando SlideShow

--- Desktop/CP2-WEB/script.js
+++ Desktop/CP2-WEB/script.js
@@ -1,4 +1,4 @@
-const colors = ['#FA8072', '#F0E68C', '#7FFFD4'];
+const colors = ['#7FFFD4', '#F0E68C', '#FA8072'];
 
 function changeBackgroundColor() {
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
@@ -10,3 +10,22 @@ changeBackgroundColor();
 
 // Define um intervalo para chamar a função a cada 5 segundos (5000 milissegundos)
 setInterval(changeBackgroundColor, 5000);
+
+
+
+const images = ['img/bikecidade.jpg', 'img/bikeneve.jpg', 'img/bikemontanha.jpg'];
+let currentIndex = 0;
+
+function changeSlide() {
+    const imageElement = document.getElementById('slideshow-image');
+    imageElement.src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+
+
+// Chama a função inicialmente para definir a primeira imagem do slideshow
+changeSlide();
+
+// Define um intervalo para chamar a função de mudar o slide a cada 5 segundos (5000 milissegundos)
+setInterval(changeSlide, 5000);
